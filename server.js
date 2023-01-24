c44a54f508e4e50a18fc66abf1c70bbfebd7e53b
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-24 14:20:52 -0300

    Web sockets

--- server.js
+++ server.js
@@ -1,30 +1,45 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const server = require('http').Server(app);
-const { v4: uuidV4 } = require('uuid');
-const io = require('socket.io')(server);
-app.set('view engine', 'ejs');
+const express = require('express')
+const app = express()
+// const cors = require('cors')
+// app.use(cors())
+const PORT = process.env.PORT || 5000;
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
+const { v4: uuidV4 } = require('uuid')
 
-app.use(express.static('public'));
+app.use('/peerjs', peerServer);
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.redirect(`/${uuidV4()}`);
-});
+    res.redirect(`/${uuidV4()}`)
+})
 
-app.get('/:room', (req, res) => {
-    res.render('rooms', { roomId: req.params.room });
-});
+app.get('/:rooms', (req, res) => {
+    res.render('rooms', { roomId: req.params.room })
+})
 
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        socket.broadcast.emit('user-connected', userId);
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+        // messages
+        socket.on('message', (message) => {
+            //send message to the same room
+            io.to(roomId).emit('createMessage', message)
+        });
+
         socket.on('disconnect', () => {
-            socket.broadcast.emit('user-disconnected', userId);
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
         })
     })
-
 })
-server.listen(5000, () => {
-    console.log('Server is listening on port 5000');
-});
+
+server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+})
